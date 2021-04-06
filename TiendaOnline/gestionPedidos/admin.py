48130c7281656b9ed0cde042d75121244246238e
irvinmarceloc <irvinmarceloc@gmail.com>
2021-04-06 02:43:36 -0400

    Comentarios

--- TiendaOnline/gestionPedidos/admin.py
+++ TiendaOnline/gestionPedidos/admin.py
@@ -2,18 +2,18 @@ from django.contrib import admin
 from gestionPedidos.models import *
 # Register your models here.
 class ClientesAdmin(admin.ModelAdmin):
-    list_display=("nombre", "direccion", "tfno")
-    search_fields=("nombre","tfno")
+    list_display=("nombre", "direccion", "tfno") #Para ver con formatos
+    search_fields=("nombre","tfno") #Para filtrar por buscador
 
 class ArticulosAdmin(admin.ModelAdmin):
     list_display=("nombre","seccion","precio")
-    list_filter=("seccion",)
+    list_filter=("seccion",) #Para filtrar  por tipo
 
 class PedidosAdmin(admin.ModelAdmin):
     list_display=("numero","fecha","entregado")
-    list_filter=("fecha",)
-    date_hierarchy="fecha"
+    list_filter=("fecha",) #Interesante filtrar por fecha
+    date_hierarchy="fecha" #Filtro como con formatos
 
-admin.site.register(Clientes, ClientesAdmin)
+admin.site.register(Clientes, ClientesAdmin) #Incluye en el panel, el segundo argumento agrega los filtros y formatos
 admin.site.register(Articulos, ArticulosAdmin)
 admin.site.register(Pedidos, PedidosAdmin)
