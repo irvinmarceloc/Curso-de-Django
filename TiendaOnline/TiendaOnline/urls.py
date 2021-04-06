7e3f9540a3cf4c8171d7008bf7860297f3cada68
irvinmarceloc <irvinmarceloc@gmail.com>
2021-04-06 16:19:05 -0400

    Filtro de busqueda de articulos

--- TiendaOnline/TiendaOnline/urls.py
+++ TiendaOnline/TiendaOnline/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
+from gestionPedidos import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^busqueda_productos/', views.busqueda_productos),
+    url(r'^buscar/', views.buscar),
 ]
--- TiendaOnline/gestionPedidos/views.py
+++ TiendaOnline/gestionPedidos/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from gestionPedidos.models import Articulos
 # Create your views here.
+def busqueda_productos(request):
+    return render(request, "busqueda_productos.html")
+
+def buscar(request):
+    if request.GET["txtBuscar"]:
+        #mensaje="Articulo buscado: %r" %request.GET["txtBuscar"]
+        producto= request.GET["txtBuscar"]
+        articulos = Articulos.objects.filter(nombre__icontains=producto)
+        return render(request, "resultados_busqueda.html", {"articulos": articulos, "query":producto})
+    else:
+        mensaje="Campo vacío, no has introducido nada"
+        return HttpResponse(mensaje)
