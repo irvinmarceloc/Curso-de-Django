bd597a8ceb87e84c92362b3935403e59c57b7631
irvinmarceloc <irvinmarceloc@gmail.com>
2021-04-06 22:00:59 -0400

    Enviar formulario por correo

--- TiendaOnline/TiendaOnline/settings.py
+++ TiendaOnline/TiendaOnline/settings.py
@@ -124,3 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_USE_TLS=True
+EMAIL_PORT=587
+EMAIL_HOST_USER='xxxx'
+EMAIL_HOST_PASSWORD='xxxxx'
