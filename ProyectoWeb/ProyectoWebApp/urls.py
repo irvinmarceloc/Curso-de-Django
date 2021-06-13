0e61497151722ea3aaf322212fc3829955d10986
irvinmarceloc <irvinmarceloc@gmail.com>
2021-06-12 22:43:29 -0400

    Rutas modificadas

--- ProyectoWeb/ProyectoWebApp/urls.py
+++ ProyectoWeb/ProyectoWebApp/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 from ProyectoWebApp import views
 
-urlpatterns = [
+urlpatterns = [ 
+    url(r'^', views.home, name="home"),
     url(r'^home/', views.home, name="home"),
     url(r'^tienda/', views.tienda, name="tienda"),
     url(r'^contacto/', views.contacto, name="contacto"),
